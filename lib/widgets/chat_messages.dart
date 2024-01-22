015270bb4aaed08f3f3726d5d3f9eb02f37a973e
debadrita205 <debadrita@technoexponent.com>
2024-01-22 18:25:31 +0530

    message display done

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (chatSnapshots.hasError) {
+          return const Center(
+            child: Text('Something went wrong.'),
+          );
+        }
+
+        final loadMessages = chatSnapshots.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadMessages.length,
+          itemBuilder: (ctx, index) => Text(loadMessages[index]['text']),
+        );
+      },
+    );
+  }
+}
