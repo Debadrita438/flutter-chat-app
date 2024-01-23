1fd2385e6820f587c25ef84f88347de96ff8da4e
debadrita205 <debadrita@technoexponent.com>
2024-01-23 18:01:30 +0530

    token wip

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,18 @@
-import 'package:chat_app/widgets/chat_messages.dart';
-import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   void _logoutHandler(BuildContext context) {
     try {
       FirebaseAuth.instance.signOut();
@@ -18,6 +25,19 @@ class ChatScreen extends StatelessWidget {
         ),
       );
     }
+  }
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+    fcm.getToken();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
   }
 
   @override
